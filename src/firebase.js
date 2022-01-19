38b5dbb3fb0f35145de48a75fc1e0d7595202a57
chrisaondo <caondo@yahoo.co.uk>
2022-01-19 12:08:21 -0600

    dependencies update

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBfClzt7gpLN2c6axjMXMon-MdD9QIyWSA",
+    authDomain: "clone-nextjs-903ba.firebaseapp.com",
+    projectId: "clone-nextjs-903ba",
+    storageBucket: "clone-nextjs-903ba.appspot.com",
+    messagingSenderId: "11840923793",
+    appId: "1:11840923793:web:8f11bf221d69dab39c3730"
+  };
