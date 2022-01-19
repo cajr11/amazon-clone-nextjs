014009ae120178baf88a157c9c1eb61b4e01b31a
chrisaondo <caondo@yahoo.co.uk>
2022-01-19 12:28:49 -0600

    rpush homeepage on logo click router added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,22 @@ import {
     ShoppingCartIcon
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router'
 
 function Header() {
     const { data: session } = useSession();
-    console.log(session);
+    const router = useRouter();
+
+    const returnHomeHandler = () => {
+        router.push('/')
+    }
 
     return (
         <header>
             {/* Top Nav */}
            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
               <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-                 <Image src={amazonHdnLogo} width={150} height={40} objectFit="contain" className="cursor-pointer"/>
+                 <Image src={amazonHdnLogo} width={150} height={40} objectFit="contain" className="cursor-pointer" onClick={returnHomeHandler}/>
               </div>
               
               {/* Search */}
