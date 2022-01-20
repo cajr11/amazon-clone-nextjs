65827bae496d70999127874d57c2ff3dcd55b56c
chrisaondo <caondo@yahoo.co.uk>
2022-01-20 09:48:22 -0600

    firebase cconfig change

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@ import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfClzt7gpLN2c6axjMXMon-MdD9QIyWSA",
-  authDomain: "amazon-clone-nextjs-nu.vercel.app	",
-  projectId: "amazon-clone-nextjs",
+  authDomain: "clone-nextjs-903ba.firebaseapp.com",
+  projectId: "clone-nextjs-903ba",
   storageBucket: "clone-nextjs-903ba.appspot.com",
   messagingSenderId: "11840923793",
-  appId: "1:11840923793:web:8f11bf221d69dab39c3730",
+  appId: "1:11840923793:web:8f11bf221d69dab39c3730"
 };
