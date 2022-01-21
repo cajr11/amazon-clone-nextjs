9378fc05e31c6ca44af88e1ccfb22cae8663cdae
chrisaondo <caondo@yahoo.co.uk>
2022-01-21 09:43:44 -0600

    project structure update

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,10 @@
+import { buffer } from 'micro';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('../../../permissions.json')
+
+export default async (req, res) => {
+    if (req.method === 'POST'){
+
+    }
+}
